b76212d415c527c39920decde5ee3a94357f85bd
MattRowee <29.matt@gmail.com>
2019-03-29 10:26:05 -0400

    created boilerplate code. Working on Auth and splash page

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,50 @@
+import { Route, Redirect } from 'react-router-dom'
+import React, { Component } from "react"
+
+class ApplicationViews extends Component {
+
+
+    state = {
+        clients: [],
+        documents: []
+    };
+
+    isAuthenticated = () => sessionStorage.getItem("credentials") !== null || localStorage.getItem("credentials") !== null;
+
+
+        componentDidMount() {
+            const newState = {}
+
+            ClientManager.getAll()
+                .then(clients => (newState.clients = clients))
+                .then(DocumentManager.getAll)
+                .then(documents => (newState.documents = documents))
+                .then(() => this.setState(newState));
+        }
+
+        render() {
+        return (
+            <div className="navBeast">
+                <Route
+                    exact
+                    path="/clients"
+                    render={props => {
+                        if (Auth0Client.isAuthenticated()) {
+                            return <ClientList {...props} clients={this.state.clients} />;
+                        } else {
+                            Auth0Client.signIn();
+                            return null;
+                        }
+                    }}
+                />
+
+                <Route
+                    path="/login"
+                    render={props => {
+                        return <Login {...props} />
+                    }} />
+                <Route exact path="/callback" component={Callback} />
+            </div>
+        )
+    }
+    export default ApplicationViews
