60be31fff112a28a8c3d4349062e85fcc91df6fa
MattRowee <29.matt@gmail.com>
2019-04-09 15:08:17 -0400

    functioning search bar filters through clients based on name

--- src/components/clients/clientList.js
+++ src/components/clients/clientList.js
@@ -2,12 +2,54 @@ import React, { Component } from 'react'
 import ResourceCard from "../generics/ResourceCard";
 
 export default class ClientList extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <section className="clients">
 
-          {this.props.clients.map(singleClient => {
+  state = {
+
+    clientItem: {},
+    search: ""
+  };
+  //Handles modal visibility and sets specific entry to state for editing
+  handleModal = client => {
+    this.setState({
+      showModal: true,
+      clientItem: client
+    });
+  };
+//Handles search bar field change
+handleFieldChange = evt => {
+  evt.preventDefault();
+  const stateToChange = {};
+  stateToChange[evt.target.id] = evt.target.value;
+  this.setState(stateToChange);
+};
+
+
+  render() {
+
+    let filteredClients = this.props.clients.filter(client => {
+      return (
+        client.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !==
+          -1
+      );
+    });
+    return (
+
+      <React.Fragment>
+        <div className="search-bar">
+        <input
+          id="search"
+          value={this.state.search}
+          placeholder="Search clients by name..."
+          onChange={this.handleFieldChange}
+        />
+
+        </div>
+
+
+        <section className="clients">
+        {filteredClients.map(singleClient => {
+
+          // {this.props.clients.map(singleClient => {
             if (singleClient.userId === sessionStorage.getItem('credentials')) {
               return <ResourceCard key={singleClient.id} resource={singleClient} route="clients" />
             }
