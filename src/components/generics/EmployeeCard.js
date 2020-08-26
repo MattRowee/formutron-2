9fadcc1039e02a638e9868443c5cad535ae6f0fd
Matthew  Rowe <matthewrowe@Matthews-MacBook-Pro.local>
2020-08-25 23:14:50 -0400

    EmployeeCard.js now has clients array in state, array populates and logs when modal opens

--- src/components/generics/EmployeeCard.js
+++ src/components/generics/EmployeeCard.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
 import Modal from 'react-bootstrap/Modal'
 import { render } from 'react-dom';
+import ClientManager from "../../modules/clientManager";
 
-
-// function TransferClient(){
-//     console.log("are we getting anywhere?");
-    
-// return(
-
-//     <Modal>
-//             {/* <Modal.Header>
-//               <Modal.Title>Modal heading</Modal.Title>
-//             </Modal.Header> */}
-//             <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-//             {/* <Modal.Footer>
-             
-//             </Modal.Footer> */}
-//           </Modal>
-// )
-    
-// }
 
 export default class EmployeeCard extends Component {
-    state = { show: false };
+    state = { 
+        show: false, 
+        clients: []
+    };
+    componentDidMount() {
+        const newState = {}
+
+        ClientManager.getAll()
+            .then(clients => (newState.clients = clients))
+            .then(() => this.setState(newState))
+    };
+
 
   showModal = () => {
     this.setState({ show: true });
+    console.log(this.state);
   };
 
   hideModal = () => {
@@ -40,12 +35,15 @@ export default class EmployeeCard extends Component {
                         <img src={this.props.resource.image} className="" />
                         <h4 className="clientCardName">{this.props.resource.name}</h4>
                         <h6 className="clientCardEmail">{this.props.resource.email}</h6>                       
-                            <button className="nav-link"  onClick={this.showModal} >Transfer a Client</button>                      
+                            <button className="nav-link"  onClick={this.showModal} >Transfer a Client</button>    
+                            
                     </h5>
                     <Modal show={this.state.show} handleClose={this.hideModal}>
-                        <p>Modal</p>
+                        <p>Which client would you like to transfer?</p>
+                        {this.state.clients.map(singleClient => {
+                                console.log(singleClient);
+                        })}
                         <button className='nav-button' onClick={this.hideModal}>Hello World</button>
-                        <p>Data</p>
                     </Modal>
                 </div>
             </div>);
